be45f5c9e24dc6f69535d80036f7e8f514380e7c
brunonelson <47126837+brunonelson@users.noreply.github.com>
2019-11-13 17:54:08 -0500

    Update nelson.js

--- scenes/nelson.js
+++ scenes/nelson.js
@@ -2,7 +2,7 @@ function nelson(){
     background('#4D6F03');
 	textSize(100);
 	textAlign(CENTER, CENTER);
-	text("Nick's Scene", width/2, height/2);
+	text("nelson's Scene", width/2, height/2);
 	text("hello", width/2, height/2 + 100);
     }
     
