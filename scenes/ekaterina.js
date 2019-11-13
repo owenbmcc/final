f007f63f60ab47eec46dc2b632fc0d3aa64ee5a9
Koheiro <ekaterina.abramovich@f1006-01-34125.bmcc.cuny.edu>
2019-11-13 17:44:56 -0500

    !

--- scenes/ekaterina.js
+++ scenes/ekaterina.js
@@ -1,6 +1,6 @@
 function ekaterina() {
-	background(51);
-	textSize(100);
+	background(100);
+	textSize(120);
 	textAlign(CENTER, CENTER);
 	text("Ekaterina's Scene", width/2, height/2);
 	text("Uhuuuu!", width/2, height/2 + 100);
