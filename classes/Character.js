6d8f5e34b9013ddeba9bacc82161d3f6645bde89
Owen Roberts <oroberts@bmcc.cuny.edu>
2019-11-18 18:28:54 -0500

    move character

--- classes/Character.js
+++ classes/Character.js
@@ -10,22 +10,5 @@ class Character extends Thing {
 	display() {
 		this.sprite.display();
 	}
-	
-	move() {
-		if (keyIsDown(RIGHT_ARROW)) {
-			this.speedX = 5;
-		} else if (keyIsDown(LEFT_ARROW)) {
-			this.speedX = -5;
-		} else {
-			this.speedX = 0;
-		}
-		
-		if (keyIsDown(DOWN_ARROW)) {
-			this.speedY = 5;
-		} else if (keyIsDown(UP_ARROW)) {
-			this.speedY = -5;	
-		} else {
-			this.speedY = 0;	
-		}
-	}
+
 }
