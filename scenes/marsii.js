4971a8dc484661d53c32f43ec5bbb862f78aeca8
Marsii <naomijcmarshall@gmail.com>
2019-11-25 18:54:46 -0500

    renames for future ease

--- scenes/marsii.js
+++ scenes/marsii.js
@@ -3,6 +3,7 @@ class marsii extends Scene {
 
     preload() {
 
+        //Main character - Astronaut
         this.walkright = loadSpriteSheet('images/marsii/astwalkr.png', 64, 128, 8);
         this.walkleft = loadSpriteSheet('images/marsii/astwalkl.png', 64, 128, 8);
         this.walkup = loadSpriteSheet('images/marsii/astwalku.png', 64, 128, 8);
@@ -15,13 +16,14 @@ class marsii extends Scene {
 
     setup() {
 
+
         const animations = {
 
+            //Main character directional and idle animation
             walkright: loadAnimation(this.walkright),
             walkleft: loadAnimation(this.walkleft),
             walkup: loadAnimation(this.walkup),
             walkdown: loadAnimation(this.walkdown),
-
             idle: loadAnimation(this.idle)
 
         };
@@ -32,74 +34,73 @@ class marsii extends Scene {
     }
 
     draw() {
-        
-  if(mouseIsPressed)
-    camera.zoom = 0.5;
-  else
-    camera.zoom = 1;
-    /*
+
+        //map maybe big, helps to have camera
+        if (mouseIsPressed)
+            camera.zoom = 0.5;
+        else
+            camera.zoom = 1;
+        /*
     //bg = this.startSet;
          //.5 zoom is zooming out (50% of the normal size)
 
     
    */
-        
+
         //camera.position.x = this.sprite.position.x;
-		//camera.position.y = this.sprite.position.y;
+        //camera.position.y = this.sprite.position.y;
 
-        
-    //background(this.startSet);
-    textSize(10);
-    background('DarkBlue');
-    fill('black');
-    textAlign(CENTER, 100);
-    text("Naomi's Scene", width / 2, height / 2);
 
-		this.character.update();
-		this.character.display();
+        //background(this.startSet);
+        textSize(10);
+        background('DarkBlue');
+        fill('black');
+        textAlign(CENTER, 100);
+        text("Naomi's Scene", width / 2, height / 2);
 
-    // user input - move character around 
+        this.character.update();
+        this.character.display();
 
-    var isWalkingR = false;
-    var isWalkingL = false;
-    var isWalkingU = false;
-    var isWalkingD = false;
+        // user input - move character around 
 
-    if (keyIsDown(RIGHT_ARROW)) {
-        this.character.speedX = 5;
-        isWalkingR = true;
-    } else if (keyIsDown(LEFT_ARROW)) {
-        this.character.speedX = -5;
-        isWalkingL = true;
-    } else {
-        this.character.speedX = 0;
+        var isWalkingR = false;
+        var isWalkingL = false;
+        var isWalkingU = false;
+        var isWalkingD = false;
+
+        if (keyIsDown(RIGHT_ARROW)) {
+            this.character.speedX = 5;
+            isWalkingR = true;
+        } else if (keyIsDown(LEFT_ARROW)) {
+            this.character.speedX = -5;
+            isWalkingL = true;
+        } else {
+            this.character.speedX = 0;
+        }
+
+        if (keyIsDown(DOWN_ARROW)) {
+            this.character.speedY = 5;
+            isWalkingD = true;
+        } else if (keyIsDown(UP_ARROW)) {
+            this.character.speedY = -5;
+            isWalkingU = true;
+        } else {
+            this.character.speedY = 0;
+        }
+
+        if (isWalkingR) {
+            this.character.changeAnimation('walkright');
+        } else if (isWalkingL) {
+            this.character.changeAnimation('walkleft');
+        } else if (isWalkingD) {
+            this.character.changeAnimation('walkdown');
+        } else if (isWalkingU) {
+            this.character.changeAnimation('walkup');
+        } else {
+            this.character.changeAnimation('idle');
+        }
+
     }
-
-    if (keyIsDown(DOWN_ARROW)) {
-        this.character.speedY = 5;
-        isWalkingD = true;
-    } else if (keyIsDown(UP_ARROW)) {
-        this.character.speedY = -5;
-        isWalkingU = true;
-    } else {
-        this.character.speedY = 0;
-    }
-
-    if (isWalkingR) {
-        this.character.changeAnimation('walkright');
-    } else if (isWalkingL) {
-        this.character.changeAnimation('walkleft');
-    } else if (isWalkingD) {
-        this.character.changeAnimation('walkdown');
-    } else if (isWalkingU) {
-        this.character.changeAnimation('walkup');
-    } else {
-        this.character.changeAnimation('idle');
-    }
-
-        //camera.position.x = this.character.positionX;
-        //camera.position.y = this.character.positionY;
-}
 
 
 }
