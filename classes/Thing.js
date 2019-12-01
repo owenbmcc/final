440f2f45bcc6f864784ff192f47c640e198c1ac0
Owen Roberts <obroberts@gmail.com>
2019-12-01 17:28:17 -0500

    Revert "There is a very weird thing"
    This reverts commit b97afb7c1ca57dce635e0aae26a5ad4fae0c6c16.

--- classes/Thing.js
+++ classes/Thing.js
@@ -1,14 +1,28 @@
 class Thing {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-		this.speedX = 0;
-		this.speedY = 0;
+	constructor(speedX, speedY) {
+		this.speedX = speedX;
+		this.speedY = speedY;
+		this.debug = false;
 	}
-    
-    
+		
 	update() {
 		this.sprite.position.x += this.speedX;
 		this.sprite.position.y += this.speedY;
+	}
+
+	get x() {
+		return this.sprite.position.x;
+	}
+
+	get y() {
+		return this.sprite.position.y;
+	}
+
+	set x(_x) {
+		this.sprite.position.x = _x;
+	}
+
+	set y(_y) {
+		this.sprite.position.y = _y;
 	}
 }
